b476b142be44ec48df81a5acb1275f2b2ca9a582
Ali Sina <ali.sina1447@gmail.com>
2024-10-26 11:35:11 -0400

    initial commit

---
+++ src/main/java/com/alisina/HomePursuit/HomePursuitApplication.java
@@ -0,0 +1,13 @@
+package com.alisina.HomePursuit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomePursuitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomePursuitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alisina/HomePursuit/HomePursuitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alisina.HomePursuit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomePursuitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
